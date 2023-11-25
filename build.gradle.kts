016c2bdfc736a94504e8d54a3d4f7bd62ca4ee40
Marcos Pereira <marcos.silva@gmail.com>
2023-11-24 23:53:00 -0500

    Many update based on Micronaut Laumch

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 import com.adarshr.gradle.testlogger.theme.ThemeType
 import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+import io.github.vacxe.buildtimetracker.reporters.markdown.MarkdownConfiguration
+import java.time.Duration
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
@@ -8,12 +10,14 @@ import java.util.Locale
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.20"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.9.20"
-    // If the project needs to add Serialization (to JSON, for example)
-    // id("org.jetbrains.kotlin.plugin.serialization") version "1.9.20"
     id("com.google.devtools.ksp") version "1.9.20-1.0.14"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("io.micronaut.application") version "4.2.0"
     id("gg.jte.gradle") version "3.1.5"
+    id("io.micronaut.aot") version "4.2.0"
+    // Apply GraalVM Native Image plugin. Micronaut already adds it, but
+    // adding it explicitly allows to control which version is used.
+    id("org.graalvm.buildtools.native") version "0.9.28"
     // Provides better test output
     id("com.adarshr.test-logger") version "4.0.0"
     // Code Coverage:
@@ -21,17 +25,25 @@ plugins {
     id("org.jetbrains.kotlinx.kover") version "0.7.4"
     // Code Inspections
     // https://detekt.dev/
-    id("io.gitlab.arturbosch.detekt") version ("1.23.3")
+    id("io.gitlab.arturbosch.detekt") version "1.23.3"
     // Task graph utility
     // https://github.com/dorongold/gradle-task-tree
     id("com.dorongold.task-tree") version "2.1.1"
-    // To generate a git.properties file containing git repository metadata
-    id("com.gorylenko.gradle-git-properties") version "2.4.1"
     // Easily add new test sets
+    // https://github.com/unbroken-dome/gradle-testsets-plugin
     id("org.unbroken-dome.test-sets") version "4.1.0"
+    // Report task timings
+    // https://github.com/vacxe/build-time-tracker
+    id("io.github.vacxe.build-time-tracker") version "0.0.4"
+    // To check dependency updates
+    // https://github.com/ben-manes/gradle-versions-plugin
+    id("com.github.ben-manes.versions") version "0.50.0"
 }
 
-val javaVersion: Int = 17
+val ci: Boolean = System.getenv().getOrDefault("CI", "false").toBoolean()
+val releasing: Boolean = System.getenv().getOrDefault("RELEASING", "false").toBoolean()
+
+val javaVersion: Int = 21
 val dockerImage: String = "ghcr.io/liber-ufpe/project-starter"
 
 val kotlinVersion: String = project.properties["kotlinVersion"] as String
@@ -51,9 +63,6 @@ application {
 
 java {
     sourceCompatibility = JavaVersion.toVersion(javaVersion)
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(javaVersion)
-    }
 }
 
 tasks.named("test", Test::class) {
@@ -77,15 +86,26 @@ tasks.withType<DockerBuildImage> {
     )
 }
 
+// See https://graalvm.github.io/native-build-tools/latest/gradle-plugin.html
 graalvmNative {
     toolchainDetection.set(false)
     binaries {
         named("main") {
+            fallback.set(false)
+            richOutput.set(true)
             buildArgs.add("--verbose")
-            // 7GB is what is available when using Github-hosted runners:
-            // https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners#supported-runners-and-hardware-resources
-            buildArgs.add("-J-Xmx7G")
-            buildArgs.add("--initialize-at-build-time=kotlin.coroutines.intrinsics.CoroutineSingletons")
+            if (ci) {
+                // A little extra verbose on CI to prevent jobs being killed
+                // due to the lack of output (since native-image creation can
+                // take a long time to complete).
+                jvmArgs.add("-Xlog:gc*")
+            }
+
+            // Do a quick/un-optimized build. The intention is to validate
+            // it is possible to create a native-image. But, if it is running
+            // a `release` job, then it must create an optimized image, hence
+            // quickBuild must be false.
+            quickBuild.set(ci && !releasing)
         }
     }
 }
@@ -95,27 +115,38 @@ micronaut {
     testRuntime("kotest5")
     processing {
         incremental(true)
-        annotations("br.ufpe.*")
+        annotations("br.ufpe.liber.*")
+    }
+    aot {
+        // Please review carefully the optimizations enabled below
+        // Check https://micronaut-projects.github.io/micronaut-aot/latest/guide/ for more details
+        optimizeServiceLoading.set(false)
+        convertYamlToJava.set(false)
+        precomputeOperations.set(true)
+        cacheEnvironment.set(true)
+        optimizeClassLoading.set(true)
+        deduceEnvironment.set(true)
+        optimizeNetty.set(true)
     }
 }
 
 jte {
-    sourceDirectory = layout.projectDirectory.dir("src/main/jte").asFile.toPath()
-    targetDirectory = layout.buildDirectory.dir("jte-classes").get().asFile.toPath()
-    trimControlStructures = true
-    packageName = "br.ufpe.liber"
+    sourceDirectory.set(file("src/main/jte").toPath())
+    targetDirectory.set(layout.buildDirectory.dir("jte-classes").get().asFile.toPath())
+    trimControlStructures.set(true)
+    packageName.set(project.group.toString())
     generate()
+    jteExtension("gg.jte.nativeimage.NativeResourcesExtension")
     jteExtension("gg.jte.models.generator.ModelExtension") {
         property("language", "Kotlin")
     }
 }
-
+// Gradle requires that generateJte is run before some tasks
 tasks.configureEach {
-    if (listOf("kspKotlin", "inspectRuntimeClasspath").contains(name)) {
+    if (name == "inspectRuntimeClasspath" || name == "kspKotlin") {
         mustRunAfter("generateJte")
     }
 }
-
 tasks.named("jar", Jar::class) {
     dependsOn.add("precompileJte")
     from(fileTree(layout.buildDirectory.file("jte-classes").get().asFile.absolutePath)) {
@@ -129,6 +160,19 @@ testlogger {
     showStackTraces = true
 }
 
+buildTimeTracker {
+    markdownConfiguration.set(
+        MarkdownConfiguration(
+            reportFile = project.reporting.file("build-times.md").absolutePath,
+            minDuration = Duration.ofMillis(0),
+            withTableLabels = true,
+            sorted = true,
+            take = Int.MAX_VALUE,
+        )
+    )
+}
+
+// Outputs releaseDate in the format `2023 Nov 24 15:20:28`.
 tasks.register("releaseDate") {
     doLast {
         println(
@@ -137,46 +181,36 @@ tasks.register("releaseDate") {
                 .format(
                     DateTimeFormatter
                         .ofLocalizedDateTime(FormatStyle.MEDIUM)
-                        .withLocale(Locale("pt-BR")),
-                ),
+                        .withLocale(Locale.Builder().setLanguage("pt-BR").build())
+                )
         )
     }
 }
 
+// Install pre-commit git hooks to run ktlint and detekt
+tasks.register("installGitHooks", Copy::class) {
+    from(layout.projectDirectory.file("scripts/pre-commit"))
+    into(layout.projectDirectory.dir(".git/hooks"))
+    fileMode = 755
+}
+
 dependencies {
+    ksp("io.micronaut:micronaut-http-validation")
     ksp("io.micronaut.serde:micronaut-serde-processor")
-    implementation("io.micronaut:micronaut-aop")
-    implementation("io.micronaut:micronaut-http-client")
-    implementation("io.micronaut.serde:micronaut-serde-jackson")
-    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
     implementation("io.micronaut.kotlin:micronaut-kotlin-extension-functions")
-
-    compileOnly("org.graalvm.nativeimage:svm")
-    runtimeOnly("ch.qos.logback:logback-classic")
-    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
-
-    // Views Dependencies
-    implementation("gg.jte:jte-kotlin:$jteVersion")
-    implementation("gg.jte:jte-runtime:$jteVersion")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
     implementation("io.micronaut.views:micronaut-views-jte")
-    jteGenerate("gg.jte:jte-models:$jteVersion")
-    jteGenerate("gg.jte:jte-native-resources:$jteVersion")
-
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    compileOnly("io.micronaut:micronaut-http-client")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+    testImplementation("io.micronaut:micronaut-http-client")
 
-    // If the project needs to add Serialization (to JSON, for example). Also check the
-    // plugins section.
-    // implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-
-    // Netty security overrides
-    implementation("io.netty:netty-handler:4.1.101.Final")
-    implementation("io.netty:netty-codec-http2:4.1.101.Final")
-
-    // Accessibility Tests
-    accessibilityTestImplementation("org.seleniumhq.selenium:selenium-java:4.15.0")
-    accessibilityTestImplementation("com.deque.html.axe-core:selenium:4.8.0")
-
-    // Kotest manual update
-    testImplementation("io.micronaut.test:micronaut-test-kotest5:4.1.0")
+    // jte dependencies
+    jteGenerate("gg.jte:jte-models:$jteVersion")
+    jteGenerate("gg.jte:jte-native-resources:$jteVersion")
+    implementation("gg.jte:jte:$jteVersion")
+    implementation("gg.jte:jte-kotlin:$jteVersion")
 }
