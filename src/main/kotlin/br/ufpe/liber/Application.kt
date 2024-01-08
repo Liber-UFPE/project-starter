ef0f556974a1710b6f755b0d9190c535f42f62cf
Marcos Pereira <marcos.silva@gmail.com>
2024-01-08 12:38:52 -0500

    style: fix all missing diktat issues

--- src/main/kotlin/br/ufpe/liber/Application.kt
+++ src/main/kotlin/br/ufpe/liber/Application.kt
@@ -1,14 +1,7 @@
 package br.ufpe.liber
 
-import gg.jte.ContentType
-import gg.jte.TemplateEngine
-import gg.jte.resolve.DirectoryCodeResolver
-import io.micronaut.context.annotation.Factory
 import io.micronaut.context.env.Environment
 import io.micronaut.kotlin.runtime.startApplication
-import jakarta.inject.Singleton
-import org.slf4j.LoggerFactory
-import java.nio.file.Paths
 
 object Application {
     @JvmStatic
@@ -20,22 +13,4 @@ object Application {
             eagerInitAnnotated(EagerInProduction::class.java)
         }
     }
-}
-
-@Factory
-internal class TemplatesFactory {
-    private val logger = LoggerFactory.getLogger(TemplatesFactory::class.java)
-
-    @Singleton
-    fun createTemplate(environment: Environment): Templates = if (devEnvironment(environment)) {
-        logger.info("Hot reloading jte templates")
-        val codeResolver = DirectoryCodeResolver(Paths.get("src/main/jte"))
-        val templateEngine = TemplateEngine.create(codeResolver, ContentType.Html)
-        DynamicTemplates(templateEngine)
-    } else {
-        logger.info("Use pre-compiled templates")
-        StaticTemplates()
-    }
-
-    private fun devEnvironment(env: Environment): Boolean = env.activeNames.contains(Environment.DEVELOPMENT)
 }
---
+++ src/main/kotlin/br/ufpe/liber/TemplatesFactory.kt
@@ -0,0 +1,28 @@
+package br.ufpe.liber
+
+import gg.jte.ContentType
+import gg.jte.TemplateEngine
+import gg.jte.resolve.DirectoryCodeResolver
+import io.micronaut.context.annotation.Factory
+import io.micronaut.context.env.Environment
+import jakarta.inject.Singleton
+import org.slf4j.LoggerFactory
+import java.nio.file.Paths
+
+@Factory
+internal class TemplatesFactory {
+    private val logger = LoggerFactory.getLogger(TemplatesFactory::class.java)
+
+    @Singleton
+    fun createTemplate(environment: Environment): Templates = if (isDevelopmentEnvironment(environment)) {
+        logger.info("Hot reloading jte templates")
+        val codeResolver = DirectoryCodeResolver(Paths.get("src/main/jte"))
+        val templateEngine = TemplateEngine.create(codeResolver, ContentType.Html)
+        DynamicTemplates(templateEngine)
+    } else {
+        logger.info("Use pre-compiled templates")
+        StaticTemplates()
+    }
+
+    private fun isDevelopmentEnvironment(env: Environment): Boolean = env.activeNames.contains(Environment.DEVELOPMENT)
+}
