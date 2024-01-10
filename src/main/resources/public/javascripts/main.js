1fc42a2411321a46fd86b4bf94ad1cb2b44d2c86
Marcos Pereira <marcos.silva@gmail.com>
2024-01-10 09:59:53 -0500

    assets: Complete assets pipeline build

--- src/main/resources/public/javascripts/main.js
+++ src/main/resources/public/javascripts/main.js
@@ -1,1 +1,12 @@
+import {Collapse} from "bootstrap";
+
+function setupCollapsableComponents() {
+    const collapseElementList = document.querySelectorAll(".collapse");
+    [...collapseElementList].forEach(collapseEl =>
+        collapseEl.addEventListener("click", () => new Collapse(collapseEl))
+    );
+}
+
+window.addEventListener("load", setupCollapsableComponents);
+
 console.error("Ooops! Ainda tem algumas coisas para customizar no template");
