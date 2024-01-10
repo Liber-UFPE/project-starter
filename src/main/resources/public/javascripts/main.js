955babcb3ea68775ee2c075c9c87ad3320e67b7c
Marcos Pereira <marcos.silva@gmail.com>
2024-01-10 14:02:09 -0500

    assets: Use optimized assets from pipeline

--- src/main/resources/public/javascripts/main.js
+++ src/main/resources/public/javascripts/main.js
@@ -1,4 +1,12 @@
 import {Collapse} from "bootstrap";
+import * as Turbo from 'https://cdn.skypack.dev/pin/@hotwired/turbo@v7.3.0-44BiCcz1UaBhgMf1MCRj/mode=imports,min/optimized/@hotwired/turbo.js';
+
+document.addEventListener("turbo:before-fetch-request", function (_e) {
+    Turbo.navigator.delegate.adapter.showProgressBar();
+});
+document.addEventListener("turbo:frame-load", function (_e) {
+    Turbo.navigator.delegate.adapter.progressBar.hide()
+});
 
 function setupCollapsableComponents() {
     const collapseElementList = document.querySelectorAll(".collapse");
@@ -9,4 +17,4 @@ function setupCollapsableComponents() {
 
 window.addEventListener("load", setupCollapsableComponents);
 
-console.error("Ooops! Ainda tem algumas coisas para customizar no template");
+console.info("Ooops! Ainda tem algumas coisas para customizar no template");
