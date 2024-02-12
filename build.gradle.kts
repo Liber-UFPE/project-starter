86a9a9c4821ccbd9ea853bdab7ad0c971ac0fc9b
Marcos Pereira <marcos.silva@gmail.com>
2024-02-12 10:34:01 -0500

    build: allow apps to customize the build

--- build.gradle.kts
+++ build.gradle.kts
@@ -287,3 +287,5 @@ dependencies {
     accessibilityTestImplementation("org.seleniumhq.selenium:selenium-java:4.17.0")
     accessibilityTestImplementation("com.deque.html.axe-core:selenium:4.8.1")
 }
+
+apply(from = "application.gradle.kts")
