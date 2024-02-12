c27c35993d93f7c253c356741b8b31600a776216
Marcos Pereira <marcos.silva@gmail.com>
2024-02-12 14:11:42 -0500

    conf: split application conf to easy sync common

--- src/test/kotlin/br/ufpe/liber/ApplicationTest.kt
+++ src/test/kotlin/br/ufpe/liber/ApplicationTest.kt
@@ -2,14 +2,25 @@ package br.ufpe.liber
 
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
-import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.client.HttpClient
+import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
 
 @MicronautTest
-class ApplicationTest(private val application: EmbeddedApplication<*>) : BehaviorSpec({
-    given("EmbeddedApplication") {
+class ApplicationTest(
+    private val server: EmbeddedServer,
+    private val context: ApplicationContext,
+) : BehaviorSpec({
+    given("Application") {
         `when`("application starts") {
-            then("server is running") { application.isRunning shouldBe true }
+            then("server is running") { server.isRunning shouldBe true }
+        }
+        `when`("has a health check") {
+            then("it should return UP") {
+                val client = context.createBean(HttpClient::class.java, server.url).toBlocking()
+                client.exchange<Any>("/health").status.code shouldBe 200
+            }
         }
     }
 })
